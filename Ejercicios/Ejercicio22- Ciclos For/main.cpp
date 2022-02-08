c9a9e5a2d6d70c43626cf769e5678ff481d4a4bd
Wilson flores <abelvilla1996@gmail.com>
2022-02-08 17:15:33 -0600

    Agregando ejercicios

---
+++ Ejercicios/Ejercicio22- Ciclos For/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	for (int indice = 1; indice <= 10; indice ++)
+	{
+		cout << "Valor de la cariable indice" <<indice << endl;
+	}
+	
+	
+	
+	return 0;
+}
